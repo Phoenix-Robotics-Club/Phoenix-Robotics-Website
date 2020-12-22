ccc0056a8a7a93a96b66a766f75cc785e2ebbb8c
Phoenix-Robotics-Club <76454450+Phoenix-Robotics-Club@users.noreply.github.com>
2020-12-22 23:23:23 +0530

    recaptcha added

--- contact.js
+++ contact.js
@@ -4,10 +4,16 @@ function mail(){
         message: document.getElementById('client_message').value,
         from_mail:document.getElementById('client_email').value,
     };
-    emailjs.send("gmail","template_oqxm3bp",parms)
-    .then(function(res){
-        console.log("Email sent",res.status);
-        document.getElementById('query').style.visibility = 'visible';
-        alert('Your query is successfully submitted.')
+    
+    if (grecaptcha.getResponse() == ""){
+        alert("Please fill the captcha");
+    } else {
+         .then(function(res){
+             emailjs.send("gmail","template_oqxm3bp",parms)
+            console.log("Email sent",res.status);
+            document.getElementById('query').style.visibility = 'visible';
+            alert('Your query is successfully submitted.')
     })
+    }
+       
 }
